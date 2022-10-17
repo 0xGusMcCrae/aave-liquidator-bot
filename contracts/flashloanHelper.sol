d442c0675f08b38439b85219e1eba17cbc899982
0xGusMcCrae <0xGusMcCrae@protonmail.com>
2022-10-17 16:10:56 -0400

    added uniswap integration

--- contracts/flashloanHelper.sol
+++ contracts/flashloanHelper.sol
@@ -6,16 +6,6 @@ import {IPoolAddressesProvider} from './IPoolAddressesProvider.sol';
 import '../interfaces/IPool.sol';
 import '../interfaces/IERC20';
 
-/*probably going to do it so I call this and it encodes params that get put throught he flashloan
- and then send back to the executeOperation fallback function and then used as the payload for a
- .call() to the liquidator contract to call the liquidation */
-
-
-//will also want a withdraw function so I can withdraw rewards - will need onlyOwner if i go that route
-//or have it automatically send rewards to the owner (probably the better option)
-
-//will either want to feed liquidator address in as a constructor param or as a getflashloan param
-//because I need to be able to access it within the executeOperation function to call it
 
 contract flashLoanHelper {
 
@@ -24,27 +14,40 @@ contract flashLoanHelper {
     address public immutable liquidator;
 
 
-    constructor(address _poolAddress, address _poolAddressesProviderAddress, address _liquidator) {
+    constructor(
+        address _poolAddress, 
+        address _poolAddressesProviderAddress, 
+        address _liquidator, 
+        address wethAddress,
+        address wbtcAddress,
+        address linkAddress,
+        address usdtAddress,
+        address usdcAddress,
+        address daiAddress
+        ) {
         pool = IPool(_poolAddress);
         poolAddressesProvider = IPoolAddressProvider(_poolAddressesProviderAddress);
         liquidator = _liquidator;
         //max approve all possible assets for aave pool spend (to repay flashloan)
         //wETH
-        IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1).approve(_poolAddress,2^256 - 1);
+        IERC20(wethAddress).approve(_poolAddress,2^256 - 1);
         //wBTC
-        IERC20(0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f).approve(_poolAddress,2^256 - 1);
+        IERC20(wbtcAddress).approve(_poolAddress,2^256 - 1);
         //LINK
-        IERC20(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4).approve(_poolAddress,2^256 - 1);
+        IERC20(linkAddress).approve(_poolAddress,2^256 - 1);
         //USDT
-        IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9).approve(_poolAddress,2^256 - 1);
+        IERC20(usdtAddress).approve(_poolAddress,2^256 - 1);
         //USDC
-        IERC20(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8).approve(_poolAddress,2^256 - 1);
+        IERC20(usdcAddress).approve(_poolAddress,2^256 - 1);
         //DAI
-        IERC20(0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1).approve(_poolAddress,2^256 - 1);
+        IERC20(daiAddress).approve(_poolAddress,2^256 - 1);
+        
     }
 
     function getFlashLoan(address debt, uint256 debtToCover, bytes params) public {
         aavePool.flashLoanSimple(address(this), debt, debtToCover, params, 0);
+        //send the profit back to bot address
+        IERC20(debt).transfer(msg.sender,IERC20(debt).balanceOf(address(this)));
     }
 
 
@@ -61,14 +64,12 @@ contract flashLoanHelper {
         IERC20(asset).transfer(liquidator,amount);
         //call the liquidate function with pre-encoded payload that was fed into getFlashLoan()
         liquidator.call(params);
-        //trade collateral received from liquidation back to borrowed debt asset on uniswap
 
-        //repay flashloan
+        // liquidator will liquidate positions, swap collateral + bonus back to debt token and send back here to be repaid
+        
         //since debt assets are pre-approved, the pool calls safeTransferFrom to take them back
         //so need to make sure I have amount+premium of asset on hand at end of function but I 
         //don't actually transfer them back to the pool
-        
-
     }
 
     function ADDRESSES_PROVIDER() public view returns (IPoolAddressesProvider) {
--- contracts/liquidator.sol
+++ contracts/liquidator.sol
@@ -5,28 +5,55 @@ pragma solidity ^0.8.0;
 import '../interfaces/IAaveL2Encoder.sol';
 import '../interfaces/IPool.sol';
 import '../interfaces/IERC20.sol';
+import '../interfaces/ISwapRouter.sol';
 
 contract Liquidator {
 
     IAaveL2Encoder public immutable aaveL2Encoder;
     IPool public immutable pool;
+    address public immutable wethAddress; //only weth is needed as a state variable because its the path intermediary in uniswap swap
+    ISwapRouter public immutable uniswapRouter;
 
-    constructor(address _poolAddress, address _aaveL2EncoderAddress) {
+    constructor(
+        address _poolAddress, 
+        address _aaveL2EncoderAddress,
+        address _uniswapRouter,
+        address wethAddress,
+        address wbtcAddress,
+        address linkAddress,
+        address usdtAddress,
+        address usdcAddress,
+        address daiAddress
+        ) {
         pool = IAavePool(_poolAddress);
         aaveL2Encoder = IAaveL2Encoder(_aaveL2EncoderAddress);
+        uniswapRouter = ISwapRouter(_uniswapRouter);
         //max approve all possible assets for aave pool spend
-        //wETH
-        IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1).approve(_poolAddress,2^256 - 1);
+       //wETH
+        IERC20(wethAddress).approve(_poolAddress,2^256 - 1);
         //wBTC
-        IERC20(0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f).approve(_poolAddress,2^256 - 1);
+        IERC20(wbtcAddress).approve(_poolAddress,2^256 - 1);
         //LINK
-        IERC20(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4).approve(_poolAddress,2^256 - 1);
+        IERC20(linkAddress).approve(_poolAddress,2^256 - 1);
         //USDT
-        IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9).approve(_poolAddress,2^256 - 1);
+        IERC20(usdtAddress).approve(_poolAddress,2^256 - 1);
         //USDC
-        IERC20(0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8).approve(_poolAddress,2^256 - 1);
+        IERC20(usdcAddress).approve(_poolAddress,2^256 - 1);
         //DAI
-        IERC20(0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1).approve(_poolAddress,2^256 - 1);
+        IERC20(daiAddress).approve(_poolAddress,2^256 - 1);
+        //Max approve all possible assets for uniswapv3 router (to swap collateral bonus back to debt token for flashloan repayment)
+        //wETH
+        IERC20(wethAddress).approve(_uniswapRouter,2^256 - 1);
+        //wBTC
+        IERC20(wbtcAddress).approve(_uniswapRouter,2^256 - 1);
+        //LINK
+        IERC20(linkAddress).approve(_uniswapRouter,2^256 - 1);
+        //USDT
+        IERC20(usdtAddress).approve(_uniswapRouter,2^256 - 1);
+        //USDC
+        IERC20(usdcAddress).approve(_uniswapRouter,2^256 - 1);
+        //DAI
+        IERC20(daiAddress).approve(_uniswapRouter,2^256 - 1);
     }
 
     
@@ -39,12 +66,20 @@ contract Liquidator {
     ) public {
         (bytes32 args1, bytes32 args2) = aaveL2Encoder.encodeLiquidationCall(collateral, debt, user, debtToCover, receiveAToken);
         pool.liquidationCall(args1, args2); 
-        //send liquidated collateral rewards back to caller of liqudiate function (flashloanHelper)
-        // is it collateral or debt that I get back??? I'm flashloaning the debt token but I think i get collateral back
-        IERC20(collateral).transfer(
-            msg.sender,
-            IERC20(collateral).balanceOf(address(this))
-        );
+        //swap liquidated collateral back to debt token and send back to flashloanHelper to be repaid
+        bytes swapPath;
+        if(collateral == wethAddress || debt == wethAddress){
+            swapPath = abi.encodePacked(collateral, 3000, debt); //3000 = 0.3% pool fee
+        } else {
+            swapPath = abi.encodePacked(collateral, 3000, wethAddress, 3000, debt);
+        }
+        uniswapRouter.exactInput(ISwapRouter.ExactInputParams({ 
+            path: swapPath,                                        
+            recipient: msg.sender,                                 
+            deadline: block.timestamp + 30,                        
+            amountIn: IERC20(collateral).balanceOf(address(this)), 
+            amountOutMin: debtToCover * 1.0075 //debtToCover + 0.05% premium (subject to change) + buffer 
+        }));
     }
 
 
